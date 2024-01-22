a95b801d892bb51b2d69de5ac2bc9f9fa4a7e662
Sadiqur057 <115367918+Sadiqur057@users.noreply.github.com>
2024-01-22 18:32:06 +0600

    hockey project initiated

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+        'c-primary': '#FF4240',
+        'c-secondary': '#131318',
+        'c-third': '#13131899',
+        'c-footer': '#13131899'
+      }
+    }
+  }
+}
